89593eb46cd995f296f1e8fa55d5fb6bfa8b80d1
wallan2 <will.allan2@outlook.com>
2025-05-26 14:29:26 -0400

    Updated loader.py for puremagic

--- src/lexos/io/loader.py
+++ src/lexos/io/loader.py
@@ -1,14 +1,14 @@
 """loader.py.
 
-Last Update: 2025-01-14
+Last Update: 2025-05-26
 Tested: 2025-01-14
 """
 
 import zipfile
 from pathlib import Path
-from typing import ClassVar, Optional, Self
+from typing import Optional, Self
 
-import magic
+import puremagic
 from docx import Document
 from pydantic import ConfigDict, validate_call
 from pypdf import PdfReader
@@ -34,13 +34,24 @@ from lexos.util import ensure_list
 class Loader(BaseLoader):
     """Loader."""
 
-    mime: ClassVar[magic.Magic] = magic.Magic(mime=True, uncompress=True)
-
     model_config = ConfigDict(arbitrary_types_allowed=True)
 
     def __init__(self):
         """Initialize the Loader."""
         super().__init__()
+
+    def _get_mime_type(self, path: Path | str, file_start: str) -> str:
+        """Get the mime type of a file.
+
+        Args:
+            path (Path | str): The path to the file.
+
+        Returns:
+            str: The mime type of the file.
+        """
+        if Path(path).suffix == ".pickle":
+            return "application/vnd.python.pickle"
+        return puremagic.magic_string(file_start, path)[0].mime_type
 
     def _load_docx_file(self, path: Path | str) -> None:
         """Load a docx file.
@@ -103,9 +114,10 @@ class Loader(BaseLoader):
             with zipfile.ZipFile(fin) as zip:
                 for info in zip.infolist():
                     try:
+                        # Get the mime type of the file
                         file_bytes = zip.read(info.filename)
                         file_start = file_bytes[:MIN_ENCODING_DETECT]
-                        mime_type = self.mime.from_buffer(file_start)
+                        mime_type = self._get_mime_type(info.filename, file_start)
                         if mime_type in VALID_FILE_TYPES:
                             text = decode(file_bytes)
                             self.paths.append(
@@ -130,7 +142,7 @@ class Loader(BaseLoader):
 
         Note: As of v2.10.5, Pydantic does not support recursive types (Self).
             As a result, this method performs its own check to see if the
-            value of `dataset` is of type `DataSet`.
+            value of `dataset` is of type `DataLoader`.
         """
         if not isinstance(dataset, DataLoader):
             raise LexosException("Invalid dataset type.")
@@ -152,12 +164,10 @@ class Loader(BaseLoader):
                 paths = [p for p in Path(path).rglob("*")]
                 self.load(paths)
             try:
+                # Get the mime type of the file
                 with open(path, "rb") as f:
                     file_start = f.read(FILE_START)
-                    mime_type = self.mime.from_buffer(file_start)
-                    # Workaround for testing
-                    if Path(path).suffix == ".pickle":
-                        mime_type = "application/vnd.python.pickle"
+                    mime_type = self._get_mime_type(path, file_start)
                     if mime_type in TEXT_TYPES:
                         self._load_text_file(path, mime_type)
                     elif mime_type in PDF_TYPES:
@@ -165,7 +175,7 @@ class Loader(BaseLoader):
                     elif mime_type in DOCX_TYPES:
                         self._load_docx_file(path)
                     elif mime_type in ZIP_TYPES:
-                        self._load_zip_file(path)
+                        return self._load_zip_file(path)
                     else:
                         self.errors.append(
                             f"Invalid MIME type: {mime_type} for file {path}."
