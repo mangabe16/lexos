29ae51ec383ff82c70b81d3cfb878084601e7337
wallan2 <will.allan2@outlook.com>
2025-06-19 14:31:41 -0400

    Implement method for finding multiple keywords

--- src/lexos/kwic/__init__.py
+++ src/lexos/kwic/__init__.py
@@ -39,7 +39,7 @@ class Kwic:
         )
     
     @validate_call(config=model_config)
-    def find_to_df(
+    def find_to_dataframe(
         doc: Doc | str,
         keyword: str | Pattern,
         ignore_case: bool = True,
@@ -56,3 +56,26 @@ class Kwic:
             pad_context=pad_context),
             columns=["Left", "Keyword", "Right"]
         )
+    
+    @validate_call(config=model_config)
+    def find_multiple_keywords(
+        doc: Doc | str,
+        keywords: Iterable[str | Pattern],
+        ignore_case: bool = True,
+        window_size: int = 50,
+        pad_context: bool = False,
+    ) -> Iterable[tuple[str, str, str, str]]:
+        """Generate KWIC results for multiple keywords in the document."""
+        all_kwic_results = []
+        for original_kw in keywords:
+            for left, found_keyword, right in kwic.keyword_in_context(
+                doc=doc,
+                keyword=original_kw,
+                ignore_case=ignore_case,
+                window_width=window_size,
+                pad_context=pad_context,
+            ):
+                all_kwic_results.append((left, found_keyword, right, str(original_kw)))
+        
+        return all_kwic_results
+    
