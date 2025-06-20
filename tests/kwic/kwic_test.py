e3e18248a2cb24c6ec159bf4d1efc2ee7c44fbf1
wallan2 <will.allan2@outlook.com>
2025-06-20 12:51:21 -0400

    Implement tests for find_multiple_keywords

--- tests/kwic/kwic_test.py
+++ tests/kwic/kwic_test.py
@@ -123,3 +123,48 @@ def test_doc_input_to_dataframe(tokenizer) -> None:
     assert list(kwic_df.columns) == ["Left", "Keyword", "Right"]
     assert kwic_df.iloc[0].tolist() == [" a ", "test", " st"]
     assert kwic_df.iloc[1].tolist() == ["to ", "test", " th"]
+
+
+
+def test_multiple_keywords(tokenizer) -> None:
+    """Test KWIC find method with multiple keywords."""
+    text = "This is a test string to test the Kwic module for finding correct words in context."
+    doc = tokenizer(text)
+    keywords = ["test", "Kwic"]
+    kwic_results = Kwic.find_multiple_keywords(
+        doc=doc, keywords=keywords, window_size=3, pad_context=True
+    )
+    results_list = list(kwic_results)
+    assert isinstance(kwic_results, Iterable)
+    assert len(results_list) == 3  
+    assert results_list[0] == (" a ", "test", " st", "test")
+    assert results_list[1] == ("to ", "test", " th", "test")
+    assert results_list[2] == ("he ", "Kwic", " mo", "Kwic")
+
+
+def test_multiple_keywords_none_provided(tokenizer) -> None:
+    """Test KWIC find method with no keywords provided."""
+    text = "This is a test string to test the Kwic module for finding correct words in context."
+    doc = tokenizer(text)
+    kwic_results = Kwic.find_multiple_keywords(
+        doc=doc, keywords=[], window_size=3, pad_context=True
+    )
+    results_list = list(kwic_results)
+    assert isinstance(kwic_results, Iterable)
+    assert results_list == []
+
+def test_mixed_string_regex_keywords(tokenizer) -> None:
+    """Test KWIC find method with a mix of string and regex keywords."""
+    text = "This is a test string to test the Kwic module for finding correct words in context using kwic."
+    doc = tokenizer(text)
+    keywords = ["test", r"[Kk]wic"]
+    kwic_results = Kwic.find_multiple_keywords(
+        doc=doc, keywords=keywords, window_size=3, pad_context=True
+    )
+    results_list = list(kwic_results)
+    assert isinstance(kwic_results, Iterable)
+    assert len(results_list) == 4  
+    assert results_list[0] == (" a ", "test", " st", "test")
+    assert results_list[1] == ("to ", "test", " th", "test")
+    assert results_list[2] == ("he ", "Kwic", " mo", "[Kk]wic")
+    assert results_list[3] == ("ng ", "kwic", ".  ", "[Kk]wic")
