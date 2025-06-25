c054e1384a801da7241c62512abf31176ba87d24
scottkleinman <scott.kleinman@csun.edu>
2025-06-25 13:22:41 -0700

    Add additional tests and docstrings

--- src/lexos/topwords/__init__.py
+++ src/lexos/topwords/__init__.py
@@ -1,3 +1,9 @@
+"""__init__.py.
+
+Last Updated: June 25, 2025
+Last Tested: June 25, 2025
+"""
+
 import pandas as pd
 from pydantic import BaseModel, ConfigDict, Field, ValidationError
 
@@ -7,4 +13,4 @@ class TopWords(BaseModel):
 
     def to_df(self):
         """Return a pandas DataFrame representation of the model."""
-        ...
+        raise NotImplementedError("Subclasses must implement the to_df method")
